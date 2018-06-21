6a7882fc9e35ca98a09e36d703d92cca0cdd4441
SEZER BAĞLAN <sezerbaglan@SEZER-MacBook-Air.local>
2018-06-21 19:14:23 +0300

    lambda expression with input arguments and capturing parameters

--- cpp11/lambda.cpp
+++ cpp11/lambda.cpp
@@ -8,13 +8,40 @@ void test(void (*ptrFunc)()){
 
 int main(){
 
-    [](){cout << "lambda expression" << endl;};
-	
+	//lambda expression without input arguments	
     auto func =  [](){cout << "lambda expression" << endl;};
-
     func();
-
     test(func);
+
+    //lambda expression with input arguments
+    auto divFunc = [](double a, double b) -> double {
+
+    	if(b == 0.0){
+    		return 0;
+    	}
+
+    	return a/b;
+    };
+
+    cout << divFunc(8,3) << endl;
+
+
+    //Capture parameters
+    int one{1};
+    int two{2};
+    int three = 3;
+
+    //Lambda expression has its own scope so capture one!
+    [one](){ cout << one << endl;}();
+
+    //Capture every local variable by copy.
+    [=](){cout << one << " " << two << " "<< three << endl;}();
+
+    //Capture every local variable by reference.
+    [&](){one = 11; cout << one << " " << two << " "<< three << endl;}();
+
+    //Capture every local variable by copy, except three.
+    [=, &three](){three = 33; cout << one << " " << two << " "<< three << endl;}();
 
     return 0;
 }
